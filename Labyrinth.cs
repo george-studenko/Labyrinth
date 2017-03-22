ac90edc2b382cc3ad9b09d0ad564739ce00ac0c7
George Studenko <gnstudenko@gmail.com>
2017-03-22 20:07:46 +0100

    feat: Add Labyrinth properties and default constructor

--- Labyrinth.cs
+++ Labyrinth.cs
@@ -8,6 +8,22 @@ namespace LabyrinthGenerator
 {
     class Labyrinth
     {
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private List<string> sides = new List<string>{ "TOP","RIGHT","BOTTOM","LEFT"};
+        public string[,] TheLabyrinth { get; set; }
 
+        private string wall = "#";
+        // Default empty constructor
+        /// <summary>
+        /// If using the default Constructor
+        /// after creating an instance of Labyrinth 
+        /// set the Rows and Columns properties
+        /// and then call to InitializeLabyrinth
+        /// </summary>
+        public Labyrinth()
+        {
+
+        }
     }
 }
